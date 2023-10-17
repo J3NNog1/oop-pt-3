50cf22e114f48742f93e395af024e789da6b66fd
jennifer j <jenniferjuarezjj@gmail.com>
2023-10-17 00:07:05 -0400

    create first recursive method

---
+++ src/FibonacciSeries.java
@@ -0,0 +1,13 @@
+public class FibonacciSeries {
+    public static void main(String[] args) {
+        int n = (34);
+        System.out.println("Fibonacci of " + n + " is " + fibonacci(n));
+    }
+    public static int fibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        } else {
+            return fibonacci(n - 1) + fibonacci(n - 2);
+        }
+    }
+}
