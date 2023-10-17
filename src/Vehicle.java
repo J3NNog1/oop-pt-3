25fd61d7c4269586dc2c2d2b56dd7189efe4c79e
jennifer j <jenniferjuarezjj@gmail.com>
2023-10-17 13:44:19 -0400

    add function to vehicle class

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -1,5 +1,22 @@
-public class Vehicle {
-    public static void main(String[] args) {
+class Vehicle {
+    private String brand;
+    private String model;
+    private int year;
+    public Vehicle(String brand, String model, int year){
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+    }
+    public void start() {
+        System.out.println("The vehicle is starting.");
+    }
+
+    public void displayInfo(){
+        System.out.println("Brand: " + brand);
+        System.out.println("Model: " + model);
+        System.out.println("Year: " + year);
+    }
+    public static void main(String [] args){
 
     }
 }
